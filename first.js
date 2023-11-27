8443126277015e76d28ee43d8b002e68f5840c54
temuu-ux <temuulenb80@gmail.com>
2023-11-27 12:58:27 +0800

    first commit

---
+++ first.js
@@ -0,0 +1,104 @@
+// let-> declare a varaible
+// = -> assign a value
+// assignment -> suulgaj ogoh
+// "trapane" -> value
+// "" -> questation mark
+// ; -> (semi colon)  end of the line
+let firstname = "trapane";
+// let myAge = 18;
+
+//console.log -> ur dung hevleh command
+// console.log(firstname);
+// console.log(myAge);
+
+// let num1 = 50;
+// let num2 = 20;
+
+// // let sum = num1 + num2;
+// let product = num1 * num2;
+// let division = num1 / num2;
+// let subtraction = num1 - num2;
+
+// console.log(num1 + num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 - num2);
+
+// // y= x*x + 2*x + 1;
+// // let x = 5;
+// // do something here
+// let answer = x * x + 2 * x + 1;
+// console.log(answer);
+
+// // declare a varaible called "number1" and "number2" and assign a value
+// // let number1 = 30;
+// let number2 = 5;
+// //and print out sum of the two varaibles
+// let sum = number1 + number2;
+// console.log(sum);
+
+// // = >right hand operator
+// // + - * / -> left hand operator
+// let number = 25;
+// number = number - number + 20;
+
+// console.log(number);
+// // shine yum
+// let x = 15;
+// let y = 20;
+// if (x > y) {
+//   console.log("x is bigger");
+// } else {
+//   console.log("y is bigger");
+// }
+
+// // let myAge = 18;
+// if (myAge > 21) {
+//   console.log("orno");
+// } else {
+//   console.log("orohgui");
+// }
+
+// let myBirthYear = 2005;
+// console.log(2023 - myBirthYear);
+
+// let myAge = 18;
+// if (myAge >= 18) {
+//   console.log("allowed", myAge);
+// } else {
+//   console.log("not allowed", myAge);
+// }
+// let myAge = 18;
+// console.log("temuulen", myAge);
+
+//data types
+// let number1 = 15;
+// let myString = "15"; //string type "text"
+// let myBool = false; // boolean type (can contain only false and true)
+// console.log("number1=", number1);
+// console.log("myString", muString);
+
+// hervee tentsuu bol
+// let myNum = 20;
+// if (myNum == 20) {
+//   console.log("mu Num is 20");
+// }
+
+// let day = 1;
+// if (day == 1) {
+//   console.log("monday");
+// } else if (day == 2) {
+//   console.log("Tuesday");
+// } else if (day == 3) {
+//   console.log("wednesday");
+// } else if (day == 4) {
+//   console.log("thursday");
+// } else if (day == 5) {
+//   console.log("friday");
+// } else if (day == 6) {
+//   console.log("saturday");
+// } else if (day == 7) {
+//   console.log("sunday");
+// } else {
+//   console.log("Not week");
+// }
